3c2b635618a2a1eb9e6c2317c9326b78bee98218
catri-01 <catrimaouene@gmail.com>
2024-01-26 09:40:42 +0100

    add architecture of project

---
+++ app/src/main/java/com/example/muko2/MainApplication.kt
@@ -0,0 +1,7 @@
+package com.example.muko2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication: Application()
---
+++ app/src/main/java/com/example/muko2/ui/favorite/FavoriteScreen.kt
@@ -0,0 +1,2 @@
+package com.example.muko2.ui.favorite
+
