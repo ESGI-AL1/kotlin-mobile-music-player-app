ee75992fa616dd3b56dbcc751c629fe7e6ed41ea
hugo-cachon <hcachon@myges.fr>
2024-02-04 15:19:15 +0100

    Adding skeletons when Deezer API is loading

--- app/src/main/java/com/example/kotlinmusic/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/kotlinmusic/ui/fragments/HomeFragment.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinmusic.R
 import com.example.kotlinmusic.network.ApiInterface
 import com.example.kotlinmusic.ui.adapters.MusicAdapter
+import com.facebook.shimmer.ShimmerFrameLayout
 import org.koin.android.ext.android.inject
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
@@ -18,31 +20,72 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 class HomeFragment : Fragment() {
 
     private val apiInterface: ApiInterface by inject()
+    private lateinit var shimmerViewContainer: ShimmerFrameLayout
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.home_fragment, container, false)
-        setupRecyclerView(view)
+        shimmerViewContainer = view.findViewById(R.id.shimmer_view_container)
+        recyclerView = view.findViewById(R.id.recyclerView)
+
+        val searchView: SearchView = view.findViewById(R.id.searchView)
+        setupSearchView(searchView)
+        fetchMusicData("eminem")
+
         return view
     }
 
-    @SuppressLint("CheckResult")
-    private fun setupRecyclerView(view: View) {
-        val recyclerView: RecyclerView? = view.findViewById(R.id.recyclerView)
-        recyclerView?.layoutManager = LinearLayoutManager(context)
+    private fun setupSearchView(searchView: SearchView) {
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.trim()?.let {
+                    if (it.isNotEmpty()) {
+                        fetchMusicData(it)
+                    }
+                }
+                return true
+            }
 
-        apiInterface.getData("eminem")
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
+    }
+
+    @SuppressLint("CheckResult")
+    private fun fetchMusicData(query: String) {
+        shimmerViewContainer.startShimmer()
+        shimmerViewContainer.visibility = View.VISIBLE
+        recyclerView.visibility = View.GONE
+
+        apiInterface.getData(query)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({ musicResponse ->
-                context?.let { safeContext ->
-                    val adapter = MusicAdapter(safeContext, musicResponse.data)
-                    recyclerView?.adapter = adapter
+                context?.let {
+                    recyclerView.layoutManager = LinearLayoutManager(it)
+                    recyclerView.adapter = MusicAdapter(it, musicResponse.data)
+                    shimmerViewContainer.stopShimmer()
+                    shimmerViewContainer.visibility = View.GONE
+                    recyclerView.visibility = View.VISIBLE
                 }
             }, { error ->
                 error.printStackTrace()
+                shimmerViewContainer.stopShimmer()
+                shimmerViewContainer.visibility = View.GONE
             })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        shimmerViewContainer.startShimmer()
+    }
+
+    override fun onPause() {
+        shimmerViewContainer.stopShimmer()
+        super.onPause()
     }
 }
